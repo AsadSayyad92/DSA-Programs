76f56d0009d331339f26eb353939095bf9fc13f7
asad github <asadsayyad233@gmail.com>
2024-08-09 12:26:12 +0530

    Queue added

---
+++ queue.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std;
+
+class Queue{
+
+public:
+    int *arr;
+    int size;
+    int qfront;
+    int rear;
+
+Queue(){
+    // initialization
+    size = 100001;
+    arr = new int[size];
+    qfront = 0;
+    rear = 0;
+}
+
+bool isEmpty(){
+    // declare empty function
+    if(qfront == rear){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void enqueue(int data){
+    if(rear == size){
+        cout<<"queue is full"<<endl;
+    }
+    else{
+        arr[rear] = data;
+        rear++;
+    }
+}
+
+int dequeue(){
+    if(rear == qfront){
+        return -1;
+    }
+    else{
+        int ans = arr[qfront];
+        arr[qfront] =-1;
+        qfront++;
+        if(rear == qfront){
+            qfront =0;
+            rear = 0;
+        }
+        return ans;
+    }
+}
+
+int front(){
+    if(qfront == rear){
+        return -1;
+    }
+    else{
+        return arr[qfront];
+    }
+}
+
+void display(){
+        cout<<"queue element is "<<endl;
+        for(int i=0;i<5;i++){
+            
+            cout<<arr[i]<<endl;
+        }
+    }
+
+};
+
+
+
+int main(){
+    
+    Queue q;
+
+    q.enqueue(5);
+    q.enqueue(6);
+    q.enqueue(6);
+    q.dequeue();
+
+    q.display();    
+    return 0;
+}
